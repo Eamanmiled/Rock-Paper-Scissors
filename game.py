5c4534ceca5f9a26dd5198250fc4c9514ee349c1
KindaBadAtThis <130555562+Eamanmiled@users.noreply.github.com>
2024-07-10 18:47:28 +0100

    Add files via upload
    the groundwork, simple intro and imported 2 libraries. From here i will create the actual gameplay and a system that reads your win loss draw record to a separate file.

---
+++ game.py
@@ -0,0 +1,23 @@
+import random, time
+
+print("hello this is my rock paper scissors game\nyou may type either of those three options or \n1\n2\n3\n"
+      "to play your hand, enter anything to begin.")
+input()
+print("------------------------------------------------------------")
+time.sleep(2)
+print("You find yourself in a dark, damp dungeon, the air thick with the smell of moss and\n"
+      "something you can't quite place... maybe dragon breath?\n"
+      "You hear a low growl and turn around, your heart racing. A massive monster emerges from\n"
+      "the shadows, its eyes glowing with an eerie light. You brace yourself for an attack,\n"
+      "gripping your sword tightly.\n")
+input("press anything to continue")
+print("But then, to your utter surprise, the monster stops just a few feet away. It tilts its\n"
+      "head, then slowly extends one of its giant claws.\n"
+      "You blink in confusion as the monster gestures for... a handshake? No, wait... it's\n"
+      "gesturing to play rock-paper-scissors!\n"
+      "You laugh nervously, unsure of what to do. But, hey, if a monster wants to play a game,\n"
+      "who are you to refuse?\n"
+      "With a sigh of relief, you put away your sword and get ready for the most intense\n"
+      "rock-paper-scissors match of your life.\n")
+choice = input("what do you choose")
+
