69269ab06daa16787fbae2ee50df4fcbd39c550a
KindaBadAtThis <130555562+Eamanmiled@users.noreply.github.com>
2024-07-11 11:29:58 +0100

    Updated game.py
    structure of complete, function started

--- game.py
+++ game.py
@@ -1,12 +1,18 @@
 import random, time
 
+def game(userinp, npcinp):
+    if userinp == 1 and npcinp == 2:
+        print("npc win")
+    elif userinp =
+
+
 print("hello this is my rock paper scissors game\nyou may type either of those three options or \n1\n2\n3\n"
-      "to play your hand, enter anything to begin.")
+      "to play your hand\nenter anything to begin.")
 input()
 print("------------------------------------------------------------")
 time.sleep(2)
 print("You find yourself in a dark, damp dungeon, the air thick with the smell of moss and\n"
-      "something you can't quite place... maybe dragon breath?\n"
+      "something  you can't quite place... maybe dragon breath?\n"
       "You hear a low growl and turn around, your heart racing. A massive monster emerges from\n"
       "the shadows, its eyes glowing with an eerie light. You brace yourself for an attack,\n"
       "gripping your sword tightly.\n")
@@ -19,5 +25,15 @@ print("But then, to your utter surprise, the monster stops just a few feet away.
       "who are you to refuse?\n"
       "With a sigh of relief, you put away your sword and get ready for the most intense\n"
       "rock-paper-scissors match of your life.\n")
-choice = input("what do you choose")
+#begin gameplay
+choices = ["1", "2", "3", "rock", "paper", "scissors"]
+choice = input("what do you choose: ")
+while choice != "run away":
+    npcchoice = choices[random.randint(0, 5)]
+    if choice not in choices:
+        choice = input("choose 1,2,3 or rock, paper, scissor please")
+    else:
+        game(choice,npcchoice)
+        choice = input("what do you choose to play now or run away?")
 
+print("thanks for playing dawg")
