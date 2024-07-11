cf513184c3a3d6814f8820ef549aea684190cedd
KindaBadAtThis <130555562+Eamanmiled@users.noreply.github.com>
2024-07-11 12:24:30 +0100

    Updated game.py
    game loop works and art found online + implemented, now just a past win/loss/draw record to be added and it will be finished

--- game.py
+++ game.py
@@ -1,13 +1,48 @@
 import random, time
 
+
 def game(userinp, npcinp):
-    if userinp == 1 and npcinp == 2:
+    if (userinp == "rock" and npcinp == "paper" or userinp == "paper" and npcinp == "scissor"
+            or userinp == "scissor" and npcinp == "rock"):
         print("npc win")
-    elif userinp =
+    elif userinp == npcinp:
+        print("Draw")
+    else:
+        print("player win")
 
 
-print("hello this is my rock paper scissors game\nyou may type either of those three options or \n1\n2\n3\n"
-      "to play your hand\nenter anything to begin.")
+def art(move):
+    if move == "rock":
+        print("""
+            _______
+        ---'   ____)
+              (_____)
+              (_____)
+              (____)
+        ---.__(___)
+        """)
+    elif move == "paper":
+        print("""
+             _______
+        ---'    ____)____
+                   ______)
+                  _______)
+                 _______)
+        ---.__________)
+        """)
+    else:
+        print("""
+            _______
+        ---'   ____)____
+                  ______)
+               __________)
+              (____)
+        ---.__(___)
+        """)
+
+
+print("hello this is my rock paper scissors game\nyou may type either of these three options"
+      " to play your hand\nenter anything to begin.")
 input()
 print("------------------------------------------------------------")
 time.sleep(2)
@@ -26,14 +61,38 @@ print("But then, to your utter surprise, the monster stops just a few feet away.
       "With a sigh of relief, you put away your sword and get ready for the most intense\n"
       "rock-paper-scissors match of your life.\n")
 #begin gameplay
-choices = ["1", "2", "3", "rock", "paper", "scissors"]
+choices = ["rock", "paper", "scissor"]
 choice = input("what do you choose: ")
 while choice != "run away":
-    npcchoice = choices[random.randint(0, 5)]
+    npcchoice = choices[random.randint(0, 2)]
     if choice not in choices:
-        choice = input("choose 1,2,3 or rock, paper, scissor please")
+        choice = input("choose rock, paper or scissor please: ")
     else:
-        game(choice,npcchoice)
-        choice = input("what do you choose to play now or run away?")
+        print("you chose", choice)
+        art(choice)
+        time.sleep(2)
+        print("Monster chose", npcchoice)
+        art(npcchoice)
+        time.sleep(2)
+        game(choice, npcchoice)
+        choice = input("what do you choose to play now or run away: ")
 
+print(
+    "                        ,////,\n"
+    "                        /// 6|\n"
+    "                        //  _|\n"
+    "                       _/_,-'\n"
+    "                  _.-/'/   \\   ,/;,\n"
+    "               ,-' /'  \\_   \\ / _/\n"
+    "               `\\ /     _/\\  ` /\n"
+    "                 |     /,  `\\_/\n"
+    "                 |     \\'\n"
+    "    /\\_        /`      /\\\n"
+    "   /' /_``--.__/\\  `,. /  \\\n"
+    "  |_/`  `-._     `\\/  `\\   `.\n"
+    "            `-.__/'     `\\   |\n"
+    "                          `\\  \\\n"
+    "                            `\\ \\\n"
+    "                              \\_\\__\n"
+    "                               \\___)\n")
 print("thanks for playing dawg")
